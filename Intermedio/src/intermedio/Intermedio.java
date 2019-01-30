9bb13baba7c88581ab5225a67436b7fcff7874da
Luis Correa <correaleyval@gmail.com>
2019-01-29 20:48:53 -0500

    Defined BufferIntermedio Object

--- Intermedio/src/intermedio/Intermedio.java
+++ Intermedio/src/intermedio/Intermedio.java
@@ -8,30 +8,12 @@ package intermedio;
  * 
  */
 public class Intermedio {
-    
-    // Definicion del objeto buffer para el proceso Intermedio
-    public static class Buffer {
-        enum ESTADO {VACIO, DESORD, ORD};
-        
-        ESTADO statevar;        
-        public static int[] fiveint;
 
-        private Buffer() {
-            this.fiveint = new int[5];
-            statevar = ESTADO.VACIO;
-        }
-    }
-    
-    
-    /**
-     * @param args the command line arguments
-     */
-    
     public static void main(String[] args) {
-        new Buffer();  // inicializar buffer      
+        new BufferIntermedio(); // inicializar buffer      
         
         System.out.println(" Proceso Intermedio");
-        System.out.println(Buffer.fiveint[0]);
+        System.out.println(BufferIntermedio.fiveint[0]);
     }
     
 }
